c2187f2a8d0a51c8034f3274f45dd1dead81c034
Veronika Scheuerová <veronikascheuerova@gmail.com>
2021-07-22 16:38:04 +0200

    fixed example config export

--- src/examples/index.ts
+++ src/examples/index.ts
@@ -5,5 +5,13 @@ import { customFeatures } from "./custom-features";
 import "./index.html";
 import "./basic-example.html";
 import "./custom-sequence-example.html";
+import { configCustomHiglightCategoryPdbIdSetting } from "./config-custom-higlights-category-pdbid";
 
-export { config, configCustomSequence, sequenceStructureMapping, sequence, customFeatures };
+export {
+    config,
+    configCustomSequence,
+    sequenceStructureMapping,
+    sequence,
+    customFeatures,
+    configCustomHiglightCategoryPdbIdSetting
+};
